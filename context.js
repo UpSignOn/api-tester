8904c251100d54186c576612959a9afa11800ff0
Gireg <gireg.dekerdanet@hotmail.fr>
2021-05-04 09:53:55 +0200

    upgrade dependencies

--- context.js
+++ context.js
@@ -1,14 +1,13 @@
-const devUrl = "http://192.168.1.11:8888/demo";
+const devUrl = 'https://isae.front1.netanswer.fr/upsignon';
 
-const buttonIds = ["SHOP1", "SHOP2"];
-
+const buttonIds = ['btn_48eafed1bd104854de02dce0eae130d3'];
 // For conversion
 const validUserWithLogin = {
-  currentLogin: "conversion@test.com", // DO NOT use test@test.com
-  currentPassword: "testConversionPass",
+  currentLogin: 'gireg.de-kerdanet', // DO NOT use test@test.com
+  currentPassword: 'totototo',
 };
 const validUserWithToken = {
-  connectionToken: "cc38b4a5-074e-420c-b560-32e4d7eabb77:cc38b4a5-074e-420c-b560-32e4d7eabb78",
+  connectionToken: 'cc38b4a5-074e-420c-b560-32e4d7eabb77:cc38b4a5-074e-420c-b560-32e4d7eabb78',
 };
 
 module.exports = {
